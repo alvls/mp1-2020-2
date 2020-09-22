c0577be1602d75c930376c1e9bb083b8f0daaafc
kde37777 <kolessnikoff2016@yandex.ru>
2020-09-22 15:50:50 +0300

    task0 ver 1.0 tested

--- kolesnikov_de/template/task0/Source.c
+++ kolesnikov_de/template/task0/Source.c
@@ -1,15 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include < math.h >
+#include <math.h>
 #include <locale.h>
 #include <stdio.h> 
 void main() {
 	setlocale(LC_ALL, "Russian");
-	float rb,rs,distance,r1,r2,x1,x2,y1,y2;
+	double rb,rs,distance,r1,r2,x1,x2,y1,y2;
     printf("Введите данные 1 окружности(x,y,r)");	
-	scanf("%f %f %f", &x1, &y1, &r1);
+	scanf("%lf %lf %lf", &x1, &y1, &r1);
 	printf("Введите данные 2 окружности(x,y,r)");
-	scanf("%f %f %f", &x2, &y2, &r2);
+	scanf("%lf %lf %lf", &x2, &y2, &r2);
 	distance = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+
+	
 	if (r1 > r2) {
 		rb = r1;
 		rs = r2;
@@ -19,30 +21,29 @@ void main() {
 		rb = r1;
 		rs = r2;
 	}
+	if ((rb == rs) & (distance == 0)) 
+	{
+		printf("Окружности совпадают");
+		return;
+	}
 	if (distance < rb - rs) 
 	{
 		printf("одна окружность лежит внутри другой");
-		return;
 	}
 	else
 	{
 		if (distance = rb - rs) {
 			printf("Окружности касаются внутренним образом");
-			return;
 			
 		}
 		else {
 			if (distance = rb + rs) {
 				printf("Окружности касаются внешним образом в 1 точке");
-				return;
 			}
 			if (rb - rs < distance < rb + rs) {
 				printf("Окружности касаются внешним образом в 2 точках");
-				return;
 			}
 			else printf("Окружности удалены друг от друга");
-			return;
-
 
 		}
 
