52872e78a37331cc19034c02a4a309b484b410a5
Sergey-Zagrebin <sergey.z.sergio@gmail.com>
2020-09-23 21:17:10 +0300

    Logic update; Inner/External touch added

--- zagrebin_sy/task0/Source.c
+++ zagrebin_sy/task0/Source.c
@@ -2,40 +2,49 @@
 
 void main() {
 
-    double x1, x2, y1, y2, r1, r2, d;
+    double x1, x2, y1, y2, r1, r2, d, sum, dif;
 
     printf("Enter  x1, x2, y1, y2 \n");
-    scanf_s("%lf %lf %lf %lf", &x1, &x2, &y1, &y2);
-     do{
+    scanf_s("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+    do {
         printf("Enter  r1, r2 \n");
         scanf_s("%lf %lf", &r1, &r2);
-     } while ((r1<=0)&&(r2<=0));
+    } while ((r1 <= 0) && (r2 <= 0));
 
-    if ((x1 == x2)&&(y1 == y2)) {
-        if (r1 == r2) {
-            printf("They are the same \n");
-            scanf_s("%lf", &d);
-            return;
+    d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    sum = (r1 + r2) * (r1 + r2);
+    dif = (r1 - r2) * (r1 - r2);
+
+        if (d > sum) {
+            printf("Too far.. \n");
+            scanf_s("%lf", x1);
         }
         else {
-            printf("One in other \n");
-            scanf_s("%lf", &d);
-            return;
+            if (d == sum) {
+                printf("External touch \n");
+                scanf_s("%lf", x1);
+            }
+            else {
+                if (d == dif) {
+                    if ((x1 == x2) && (y1 == y2)) {
+                        printf("They are the same \n");
+                        scanf_s("%lf", x1);
+                    }
+                    else {
+                        printf("Inner touch \n");
+                        scanf_s("%lf", x1);
+                    }
+                }
+                else {
+                    if (d < dif) {
+                        printf("One in other \n");
+                        scanf_s("%lf", x1);
+                    }
+                    else {
+                        printf("Crossed \n");
+                        scanf_s("%lf", x1);
+                    }
+                }
+            }
         }
-    }
-
-    d = (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1);
-
-    if (d > (r1 + r2)*(r1 + r2)) {
-        printf("Too far.. \n");
-    }
-    else {
-        if (d == (r1 + r2)*(r1 + r2)) {
-            printf("Touched \n");
-        }
-        else {
-            printf("Crossed \n");
-        }
-    }
-    scanf_s("%lf", &d);
 }
