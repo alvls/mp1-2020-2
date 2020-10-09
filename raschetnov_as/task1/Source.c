721b291ac0a54f3d35e2713a2e2f21bca447ec67
alser3217 <raschetnov.alexei@gmail.com>
2020-10-09 10:52:27 +0300

    fixed2

--- raschetnov_as/task1/Source.c
+++ raschetnov_as/task1/Source.c
@@ -17,24 +17,24 @@ void main()
 	{	
 		printf("Not a number or numbers are out of range, try again:\n");
 		while((c = getchar()) != '\n' && c != EOF);
-			{
-				k = scanf("%f %f %f", &h, &w, &d);
-			}		
+		{
+			k = scanf("%f %f %f", &h, &w, &d);
+		}		
 	} 
-	h = h/100;
-	w = w/100;
-	d = d/100;
-	V1 = h*w*depth_1;
-	V2 = 2*(h*d*depth_2);	
-	V3 = 2*(w*d*depth_2);
-	V4 = h*w*depth_3;
-	count = h/(0.4);
-	V5 = count*(d*w*depth_4);
+	h = h / 100;
+	w = w / 100;
+	d = d / 100;
+	V1 = h * w * depth_1;
+	V2 = 2 * (h * d * depth_2);	
+	V3 = 2 * (w * d * depth_2);
+	V4 = h * w * depth_3;
+	count = h / (0.4);
+	V5 = count * (d * w * depth_4);
 	W1 = V1 * DENSITY_DVP;
 	W2 = V2 * DENSITY_DSP;
 	W3 = V3 * DENSITY_DSP;
 	W4 = V4 * DENSITY_WOOD;
 	W5 = V5 * DENSITY_DSP;
-	locker_weigth = W1+W2+W3+W4+W5;
+	locker_weigth = W1 + W2 + W3 + W4 + W5;
 	printf("Locker weigth is: %.2f", locker_weigth);
 }
