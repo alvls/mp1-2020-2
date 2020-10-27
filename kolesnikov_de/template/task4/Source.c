38db9da6411e7508c7c7e17462c9a79c461aab25
kde37777 <kolessnikoff2016@yandex.ru>
2020-10-28 01:10:36 +0300

    task4 second build(v 1.0)

--- kolesnikov_de/template/task4/Source.c
+++ kolesnikov_de/template/task4/Source.c
@@ -5,13 +5,13 @@ productcount[NumOfProducts] = { 0 }, barcodeout[NumOfProducts] = { 0 };
 
 //SETTING DATA
 int index[] = { 4343,5124,3213,4342,6543,6542,6876,5323,2314,9876 };
-int pricelist[] = { 1543,1654,7654,2434,5453,434,545,6325,9876 };
+int pricelist[] = { 1543,1654,7654,2434,5453,434,545,6325,9876, 4353};
 char description[] = ("Smart Digital Picture Frame 10.1Multipurpose Foldable Computer Study Desk - Black"
 	"2-Piece Melamine Dinnerware Set"
 	"Moving Boxes with Handles"
 	"Dual Performance Bath Towel"
 	"Foldable Clothes Drying Laundry Rack"
-	"AmazonBasics Replacement Water Filters for Water Pitchers"
+	"Replacement Water Filters for Water Pitchers"
 	"Memory Foam With Strong Innerspring Support"
 	"Kitchen Storage Baker's Rack with Wood Table"
 	"Step Hair Dryer And Volumizer Hot Air Brush");
@@ -20,8 +20,6 @@ int descriptionsize[NumOfProducts][2] = {
 	{205,263} ,{263,307} ,{307,345} ,{345,388}
 };
 float discountlist[] = {10,25,45,15,20,10,5,5,90,50 };
-
-
 
 
 
@@ -38,20 +36,20 @@ int summator(int code,int count) {
 	return((temp1-(temp1 * (discountlist[code] / 100))*count));
 }
 void FinalSum(int mode) {
-	for (k,i = 0; i < NumOfProducts,k<countproducts; i++,k++) {
+	for (i = 0; i < NumOfProducts; i++) {
 		if (barcodeout[i] == 1) {
 			temp1 = pricelist[i];
 			temp2 = productcount[i];
 			sum += temp1*temp2;
-			sumd += temp1*(discountlist[i]/100);
-			totalsum += (sum - sumd/100)*temp2;
+			sumd += temp1*(discountlist[i]/100)*temp2;
+			totalsum = (sum - sumd);
 		}
 	}
 	if (mode == 1) {
-		printf("Sum without diskount`s = %d, sum of discounts = %d, total sum = %d", sum, sumd, totalsum);
+		printf("Sum without diskount`s = %d, sum of discounts = %d, total sum = %d\n", sum, sumd, totalsum);
 	}
 	else
-		printf("Total sum = %d", totalsum);
+		printf("Total sum = %d\n", totalsum);
 
 }
 void seedescription() {
@@ -80,7 +78,7 @@ void scanproduct() {
 
 }
 void makeout() {
-	for (k=0,i = 0; i < NumOfProducts,k<countproducts; i++,k++) {
+	for (i = 0; i < NumOfProducts; i++) {
 		if (barcodeout[i] == 1){
 			temp1 = descriptionsize[i][0];
 			temp2 = descriptionsize[i][1];
@@ -119,6 +117,7 @@ int main() {
 			break;
 		case 4:
 			makeout();
+			printf("\n");
 			break;
 		case 5:
 			FinalSum(0);
