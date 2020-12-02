18c0cf461ab7c8be1fba76846c51845037bf96aa
Sergey-Zagrebin <sergey.z.sergio@gmail.com>
2020-12-02 17:41:37 +0300

    Task 4 corrected. Task 5 already commited

--- zagrebin_sy/task4/Source.c
+++ zagrebin_sy/task4/Source.c
@@ -1,48 +1,61 @@
-/*#include <stdio.h>
-#define SIZE 4
+#include <stdio.h>
+#define SIZE 5	// number of goods
 
-int codes[SIZE] = { 1234, 4321, 234, 1515};
-char names[SIZE][10] = { "9 eggs " , "milk ", "bread ", "apple " };
-double prices[SIZE] = { 99, 65, 35, 100 };
-double discounts[SIZE] = { 0.15, 0.1, 0, 0.15 };
-char purchase[SIZE] = {0};
+char* codes[SIZE] =	   { "0000","1234",      "4321",  "0234",   "1515" };
+char names[SIZE][10] = { "stop", "9 eggs " , "milk ", "bread ", "apple " };
+double prices[SIZE] =  {  0,      99,          65,      35,       100 };
+double discounts[SIZE] = { 0,     0.15,        0.1,     0,        0.15 };
+char purchase[SIZE] = { 0 };
 double bill = 0;
 double discount = 0;
 
-void info(int code) {
-	int res = 0;
-	int i;
-	for (i = 0; i < SIZE; i++) {
-		if (codes[i] == code) {
-			res = 1;
-			purchase[i]++;
-			bill += prices[i];
-			discount += discounts[i] * prices[i];
-			break;
-		}
-	}	
-	if (res == 1) {
-		printf("%s count:%.2lf  price : %.2lf \n",names[i], discounts[i], prices[i] * (1-discounts[i]));
+
+int find_item(char* code) {
+	int j;
+	for (int i = 0; i < SIZE; i++) {
+		for (j = 0; j < 4; j++)
+			if (code[j] != codes[i][j])
+				break;
+		if (j == 4)
+			return i;
 	}
-	else
-		printf("No such item \n");
+	return -1;
 }
 
 void main() {
-	int code;
+	char code[5];
+	char ñ = 0;
+	int res, next = 1;;
 
-	printf("Welcome to Pyatorochka \nType -1 to exit \n");
-	scanf_s("%d", &code);
-	while (code != -1) {
-		info(code);
-		scanf_s("%d", &code);
+	while (next) {
+		system("cls");
+		printf("Welcome to Pyatorochka\n\n");
+		for (int i = 0; i < SIZE; i++)
+			printf("%-6.6s - %4.4s\n", names[i], codes[i]);
+		
+		gets(code);
+		res = find_item(code);
+		while (res) {
+			if (res == -1)
+				printf("No such item\n");
+			else {
+				purchase[res]++;
+				bill += prices[res];
+				discount += discounts[res] * prices[res];
+				printf("%s count:%.2lf  price : %.2lf \n", names[res], discounts[res], prices[res] * (1 - discounts[res]));
+			}
+			gets(code);
+			res = find_item(code);
+		};
+		system("cls");
+		printf("Your bill:\n");
+		for (int i = 0; i < SIZE; i++)
+			if (purchase[i] != 0)
+				printf("%s price: %.2lf  count: %d  full price: %.2lf\n", names[i], prices[i] * (1 - discounts[i]), purchase[i], prices[i] * (1 - discounts[i]) * purchase[i]);
+		printf("\nTotal:\nfull price: %.2lf  discount: %.2lf  to pay: %.2lf", bill, discount, bill - discount);
+		printf("\nHope to see you soon! Come back!\n");
+		printf("\nEnter 0 to turn off the cashbox or any other number to continue\n");
+		scanf_s("%d", &next);
 	}
-
-	printf("Your bill: \n");
-	for (int i = 0; i < SIZE; i++)
-		if (purchase[i] != 0)
-		printf("%s price: %.2lf  count: %d  full price: %.2lf\n", names[i], prices[i] * (1-discounts[i]), purchase[i], prices[i] * (1 - discounts[i])* purchase[i]);
-	printf("Total: full price: %.2lf  discount: %.2lf  to pay: %.2lf", bill, discount, bill-discount);
-	scanf_s("%d", &code);
+	system("pause");
 }
-*/
