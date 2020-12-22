5fd2082e0837d1f2359be655be6065a3129f1547
DDYunin <DaniilUnin2002@yandex.ru>
2020-12-22 22:51:57 +0300

    Исправил Лабораторную работу №6, так как сегодня крайний день сдачи

---
+++ yunin_dd/task6/SecondMode.h
@@ -0,0 +1,13 @@
+#ifndef ___SECONDMODE_H___
+#define ___SECONDMODE_H___
+
+int CalculateAndPrintDataForCoth(double etal, double* tempmno, int countexperim, double x);
+int CalculateAndPrintDataForCos(double etal, double* tempmno, int countexperim, double x);
+int CalculateAndPrintDataForExp(double etal, double* tempmno, int countexperim, double x);
+int CalculateAndPrintDataForCh(double etal, double* tempmno, int countexperim, double x);
+int CalculateAndPrintDataForSh(double etal, double* tempmno, int countexperim, double x);
+int CalculateAndPrintDataForSin(double etal, double* tempmno, int countexperim, double x);
+int SecondFunctionMode();
+
+
+#endif 
