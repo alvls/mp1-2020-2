276b12236d321fef96357642e2f4ae01f25d6506
a1xsa <tsvetkov-alexey@mail.ru>
2020-12-29 18:17:08 +0300

    task 6

---
+++ tsvetkov.aa/task6.1/еуф/еуф/Funct.h
@@ -0,0 +1,10 @@
+#ifndef __FUNCT_H__
+#define __FUNCT_H__
+double siny(double x, int i);
+double cosy(double x, int i);
+double expy(double x, int i); 
+double sinhy(double x, int i);
+double sinhy(double x, int i);
+double factorial(int n);
+void funct(int m, int n, double t, int k, double x,double kx, double (*fun)(double, int));
+#endif
